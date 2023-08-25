f975a490e1de3c98f410e587972a5ba9397298e2
Charlie Li <charlie.li711935@gmail.com>
2023-08-25 17:31:12 -0400

    fixed text alginment

--- elk-site/src/Home.js
+++ elk-site/src/Home.js
@@ -1,16 +1,28 @@
 const Home = () => {
     return (  
         <div className="home">
-            <div className="title">
-                <h2>Leverage Test Automation and CI/CD Expertise for "Rapid Quality Assurance"</h2>
+            <div className="half">
+                <div className="half_package">
+                    <div className="title">
+                        <div>
+                            <h2>Leverage <span className="key_words">Test Automation</span> and <span className="key_words">CI/CD Expertise</span> for <span className="key_words">"Rapid Quality Assurance"</span></h2>
+                        </div>
+                    </div>
+                    <div className="content">
+                        <h3>ELK provides testing capabilities, knowledge, technology and consulting to companies
+                            looking to build and enhance automated quality assurance and improve asset data transparency. 
+                            The next generation of automation is autonomous test creation, test execution and learning, 
+                            all of which we can offer our customers.</h3>
+                    </div>
+                </div>
             </div>
-            <div className="content">
-                <h3>ELK provides testing capabilities, knowledge, technology and consulting to companies
-                     looking to build and enhance automated quality assurance and improve asset data transparency. 
-                      The next generation of automation is autonomous test creation, test execution and learning, 
-                      all of which we can offer our customers.</h3>
+            <div className="half">
+                <div className="half_package">
+                    <div className="picture">
+                        <h2>Photo</h2>
+                    </div>
+                </div>
             </div>
-            <div className="picture"></div>
         </div>
     );
 }
