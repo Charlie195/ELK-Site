06862382c821cb537e08da872a0b3f03b74568cd
Charlie Li <charlie.li711935@gmail.com>
2023-08-28 18:35:53 -0400

    added computer circuit board image and get started component

---
+++ elk-site/src/Get_Connected.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+const Get_Connected = () => {
+    return (  
+        <Link to="/contact" className="get_connected_button"><h2>Get Started</h2></Link>
+    );
+}
+ 
+export default Get_Connected;
