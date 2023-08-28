e32bbbeda975bf4820b167af872fcb398842a310
Charlie Li <charlie.li711935@gmail.com>
2023-08-28 17:55:42 -0400

    Added Get Started button

--- elk-site/src/App.js
+++ elk-site/src/App.js
@@ -1,12 +1,13 @@
 import Heading from "./Heading";
 import Home from "./Home";
+import Get_Connected from "./Get_Connected";
 
 function App() {
     return (
         <div className="App">
             <Heading />
-            
             <Home />
+            <Get_Connected />
         </div>
     );
 }
