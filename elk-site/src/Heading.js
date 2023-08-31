55e6f78204a060c59987bdcae02a7dba348691b3
Charlie Li <charlie.li711935@gmail.com>
2023-08-31 01:33:09 -0400

    version one of services tab complete, issue with colour and placement of demo buttons

--- elk-site/src/Heading.js
+++ elk-site/src/Heading.js
@@ -10,7 +10,7 @@ const Heading = () => {
                     <Link to="/services">Services</Link>
                     <Link to="/projects">Projects</Link>
                 </div>
-                <Link to="/contact" className="contact"><button>Contact</button></Link>
+                <Link to="/contact" className="contact blue_button"><button>Contact</button></Link>
             </nav>
             <div className="banner">
                 <h1>Early Learning Kingdom</h1>
